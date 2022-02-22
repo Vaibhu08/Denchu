54ec4e1a852f66990b95f57e1189633b43c09657
Chingkheinganba Irungbam <99580046+Chingkheinganba06@users.noreply.github.com>
2022-02-22 23:28:37 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let path = require('path')
 let fetch = require('node-fetch')
 let levelling = require('../lib/levelling')
-const thumb = fs.readFileSync('./src/thumb.jpg')
+const thumb = fs.readFileSync('./src/tenor.gif')
 let tags = {
   'main': 'MAIN',
   'info': 'INFO',
