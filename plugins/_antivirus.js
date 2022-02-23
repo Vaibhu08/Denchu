4f04cdf0941de17ea09477fc64459d9a6c30ceff
Chingkheinganba Irungbam <99580046+Chingkheinganba06@users.noreply.github.com>
2022-02-23 14:23:30 +0530

    Add files via upload

---
+++ plugins/_antivirus.js
@@ -0,0 +1,15 @@
+let handler = m => m
+
+handler.all = async function (m) {
+    /* 
+    automatically clears the conversation when there is an OVERSIZED message
+    https://github.com/adiwajshing/Baileys/blob/3507f8f/WAMessage/WAMessage.d.ts#L18605
+    */
+    if (m.messageStubType === 68) {
+        await this.modifyChat(m.chat, 'clear', {
+            includeStarred: false
+        }).catch(console.log)
+    }
+}
+
+module.exports = handler
